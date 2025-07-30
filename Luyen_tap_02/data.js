f9cb0eeb00988631bf98f6656721e8c66d545fb0
viet <nguyenmanhviet281@gmail.com>
2025-07-31 00:40:11 +0700

    Create data.js

---
+++ Luyen_tap_02/data.js
@@ -0,0 +1,46 @@
+const transactions = [
+    {
+        id: 1152,
+        customer: "Võ Hoài An",
+        employee: "Mai Thục Anh",
+        amount: 250000,
+        date: "06 Tháng 9 2024 9:00",
+        result: ""
+    },
+    {
+        id: 1199,
+        customer: "Hoàng Thị Thắng",
+        employee: "Nguyễn Văn Hồng",
+        amount: 600000,
+        date: "06 Tháng 9 2024 9:10",
+        result: ""
+    },
+    {
+        id: 1239,
+        customer: "Nguyễn Huy Quang",
+        employee: "Nguyễn Văn Hồng",
+        amount: 934000,
+        date: "06 Tháng 9 2024 9:12",
+        result: ""
+    },
+    {
+        id: 1677,
+        customer: "Huỳnh Văn Nam",
+        employee: "Mai Thục Anh",
+        amount: 150000,
+        date: "06 Tháng 9 2024 9:15",
+        result: ""
+    },
+    {
+        id: 1439,
+        customer: "Nguyễn Hồng Minh",
+        employee: "Nguyễn Hồng Minh",
+        amount: 354000,
+        date: "06 Tháng 9 2024 9:14",
+        result: ""
+    }
+];
+
+
+
+
